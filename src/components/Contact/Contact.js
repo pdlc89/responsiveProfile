64a70b1e874cbbd717d717c34cbb9c3cb993a1c1
pdlc89 <pcruz9332@gmail.com>
2018-04-30 20:41:09 -0700

    removed locked files

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -19,26 +19,26 @@ class Contact extends React.Component{
     })
   }
   render(){
-  const elm = <div className="ME">
+  <div className="ME">
     <button className="conRend">Contact Info</button>
     </div>
     if(this.state.isClicked) {
-    return(
-    <div className="ME">
-      <img className="meEating" alt="button" src={Me} />
-      <p className="number">Email: <b>pcruz9332@gmail.com</b></p>
-      <p className="number">Cell: <b>(714) 272-6549</b></p>
-      <p className="shortWords"></p>
-    </div>
-    )
-  }else {
-    return (
-      <div className="ME">
-      <button className="myButton"onClick={this.handleClick} checked="True">Click Here To View Contact Information</button>
-      </div>
-    )
-  }
-  }
+       return(
+          <div className="ME">
+            <img className="meEating" alt="button" src={Me} />
+            <p className="email">Email: <b>pcruz9332@gmail.com</b></p>
+            <p className="number">Cell: <b>(714) 272-6549</b></p>
+            <p className="shortWords"></p>
+          </div>
+              )
+    }else {
+        return (
+            <div className="ME">
+            <button className="myButton"onClick={this.handleClick} checked="True">Click Here To View Contact Information</button>
+            </div>
+              )
+          }
+        }
 }
 export default Contact;
 
